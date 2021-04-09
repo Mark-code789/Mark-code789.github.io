e9717555f983ef3ffd3853ffc1015e83bbead18f
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2021-04-09 17:51:15 +0300

    update Checkers AI Player.js

--- Checkers AI Player.js
+++ Checkers AI Player.js
@@ -193,7 +193,7 @@ class AI {
         let count = 0;
         
         if(moves.length > 1 && this.depth > 1) { 
-        	if(window.Worker && (this.depth > 3 || Game.version == "international")) { try {
+        	if(window.Worker && (this.depth > 3 || Game.version == "international")) {
         		worker = new Worker("Checkers Web Worker.js");
         		worker.onmessage = message;
         		Notify("Working fine");
@@ -224,7 +224,6 @@ class AI {
         		let random = Math.round(Math.random() * (bestPossibleMoves.length - 1));
 		        bestMove = bestPossibleMoves[random];
         		return Prms(bestMove);
-        		} catch (error) {alert(error)}
        	 } 
         	else {
         		for(let i = 0; i < moves.length; i++) {
