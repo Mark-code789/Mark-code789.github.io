abd80e85346b95e358996b9d5c97b78423c4076a
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2021-04-12 12:12:27 +0300

    update Checkers UI Controller.js

--- Checkers UI Controller.js
+++ Checkers UI Controller.js
@@ -3279,7 +3279,9 @@ async function back (undo = false, isComp = false) {
         let moving = $$("#transmitter .outer");
         if(moving.length == 0) {
             let length = BackState.moves.length;
-            if(length > 0 && Game.mode === "two-player-offline" || ((Game.mode == "two-player-online" || Game.mode === "single-player") && length > 1 && (Game.whiteTurn && playerA.pieceColor === "White" || !Game.whiteTurn && playerA.pieceColor === "Black") )) {
+            if(length > 0 && Game.mode === "two-player-offline" || (isComp || (Game.mode == "two-player-online" || Game.mode === "single-player") && length > 1 && (Game.whiteTurn && playerA.pieceColor === "White" || !Game.whiteTurn && playerA.pieceColor === "Black") )) {
+            	if(isComp)
+            		Notify(playerB.name + " undone the move");
                 let move = BackState.moves[length-1];
                 await BackState.moves.pop();
                 Undo.move(move);
