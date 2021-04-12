c3eb00ed2ca520c003fa071bb29ad7dac4612d2f
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2021-04-12 17:49:26 +0300

    update Checkers Channel.js

--- Checkers Channel.js
+++ Checkers Channel.js
@@ -464,7 +464,7 @@ const Message = async (prop) => { try {
             bubble.classList.add("same_side_bubble");
         } 
         container.insertBefore(bubble, anchor);
-        pTick.id = $(".right_bubble").length + "";
+        pTick.id = $$(".right_bubble").length + "";
         
         let unreadBubble = $(".center_bubble");
         if(unreadBubble != null) 
@@ -608,11 +608,23 @@ const ChangeTextBox = async (isFocused, elem) => {
 	if($(".send_button") === document.activeElement) {
 		Message({action: "send"});
 	} 
-	else if($(".chat_field") === document.activeElememt) {
+	else if(elem == $(".chat_field") && isFocused) {
 		Lobby.PUBNUB.setState({
 			state: {"isTyping": true}, 
 			channels: [Lobby.CHANNEL]
-		}, function (status, response) {});
+		}, function (status, response) {
+			if(status.error)
+				alert(error);
+		});
+	} 
+	else if(elem == $(".chat_field") && !isFocused) {
+		Lobby.PUBNUB.setState({
+			state: {"isTyping": false}, 
+			channels: [Lobby.CHANNEL]
+		}, function (status, response) {
+			if(status.error)
+				alert(error);
+		});
 	} 
 	if(!elem.className.includes("chat_field")) {
 		elem.scrollIntoView(false);
