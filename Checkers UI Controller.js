10e414dde42a846e32dad9c5e9cc1faa4a32856e
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2021-04-21 09:10:32 +0300

    update Checkers UI Controller.js

--- Checkers UI Controller.js
+++ Checkers UI Controller.js
@@ -1437,6 +1437,7 @@ const ValidateMove = async (prop) => {
            if(!isEmpty) {
                 Game.possibleMoves = await AssesMoves({id, i: prop.i, j: prop.j, state: Game.state});
                 if(Game.possibleMoves.length > 0) {
+                    alert(Game.helper + "," + other.aiPath.length);
                     if(Game.helper && other.aiPath.length == 0) {
                         for(let move of Game.possibleMoves)
                             $("#table").rows[parseInt(move.empty.slice(0,1))].cells[parseInt(move.empty.slice(1,2))].classList.add("hint");
