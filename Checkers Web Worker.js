076955533f4744179e90b9ba6fd9bfad2dc28249
Mark Etale <66547466+Mark-code789@users.noreply.github.com>
2021-04-30 10:50:08 +0300

    update Checkers Web Worker.js

--- Checkers Web Worker.js
+++ Checkers Web Worker.js
@@ -3,19 +3,16 @@ importScripts("Checkers AI Player.js");
 importScripts("Checkers Core Engine.js");
 				
 onmessage = async (e) => { try {
-	let state = e.data[0];
-	let level = e.data[1];
-	let mainMoves = e.data[2];
-	let move = e.data[3];
-	let cloneState = e.data[4];
-	let moves = e.data[5];
-	let depth = e.data[6];
-	let isMax = e.data[7];
-	let alpha = e.data[8];
-	let beta = e.data[9];
-	let currentPlayer = e.data[10];
-	HashTable.ZobristTable = e.data[11];
-	let ai = new AI({state, depth: level, moves: mainMoves});
+	let level = e.data[0];
+	let move = e.data[1];
+	let cloneState = e.data[2];
+	let moves = e.data[3];
+	let depth = e.data[4];
+	let isMax = e.data[5];
+	let alpha = e.data[6];
+	let beta = e.data[7];
+	let currentPlayer = e.data[8];
+	let ai = new AI({state: [], depth: level, moves: []});
 	let value = await ai.minimax(cloneState, moves, depth, isMax, alpha, beta, currentPlayer);
 	postMessage({title: "value", content: {move, value}});
 	} catch (error) {
